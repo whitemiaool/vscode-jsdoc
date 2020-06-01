935379216be25eec54664171f22676cd982a3a0d
dyxuan <cdduyingxuan1@jd.com>
2020-06-01 14:19:11 +0800

    feat(plugin): 新增快捷键及支持箭头函数

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ gen jsdoc
 ## Features
 
 选中一个函数的参数, `ctrl+shift+p` 后输入 `gen jsdoc` 并选择 `gen jsdoc` 命令, 即可生成 jsdoc 风格的函数注释
+或
+使用快捷键ctrl+alt+D
 
 ## Requirements
 
@@ -14,7 +16,16 @@ none
 
 This extension contributes the following settings:
 
-- `jsdoc.author`: 添加此选项, jsdoc 将自动插入作者
+```
+{
+    "jsdoc":{
+        "author":"YOUR NAME",   //添加此选项, jsdoc 将自动插入作者
+        "disDate":false,     //是否隐藏日期
+        "disType":false,    //是否隐藏参数type
+        "disReturn":false,  //是否隐藏函数return
+    }
+}
+```
 
 ## Known Issues
 
@@ -26,5 +37,5 @@ none
 
 ### 1.0.0
 
-jsdoc 1.0 正式发布!
+jsdoc this 1.0 正式发布!
 
--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ export const genJSDoc = () => {
   // 获取 selection 对象(其中包含当前选择的行与字符)
   const selection = editor.selection
   // 获取选中的内容
-  const selectionText = editor.document.getText(selection)
+  const selectionText = editor.document.lineAt(selection.start.line+1).text;
   const getParamReg = /\(([^)]*)\)/
   // 获取参数列表, 去除其中的空格与回车
   const m = selectionText.match(getParamReg)
@@ -22,31 +22,31 @@ export const genJSDoc = () => {
 
   editor.edit(editBuilder => {
     // 取上一行的末尾作为插入点
-    const selectionLine = editor.document.lineAt(selection.start.line)
-    const insertPosition = selectionLine.range.start
+    const selectionLine = editor.document.lineAt(selection.start.line+1)
+    const insertPosition = editor.document.lineAt(selection.start.line).range.start
     let text = '/**\r'
-    text += `* 描述\r`
+    text += `* @Description \r`
     // 作者
     const configuration = vscode.workspace.getConfiguration('jsdoc');
     const author: string = configuration.get('author') || ''
     author && (text += `* @author ${author}\r`)
     // 日期
-    text += `* @date ${getFormatDate('YYYY-MM-DD',new Date())}\r`
+    !configuration.get('disDate')&&(text += `* @date ${getFormatDate('YYYY-MM-DD',new Date())}\r`)
     // 参数
+    const disType = configuration.get('disType')
     text += paramList
-      .map(paramName => `* @param {any} ${paramName}\r`)
+      .map(paramName => `* @param ${disType?'':`{any} `}${paramName}\r`)
       .join('')
     // 返回值
-    text += `* @returns {any}\r`
+    !configuration.get('disReturn')&&(text += `* @returns {any}\r`)
     text += `*/\r`
-
     // 填充行头的空格
     const whitespace = selectionLine.firstNonWhitespaceCharacterIndex
     const padSpaceStr = ' '.repeat(whitespace)
     text = text.replace(/\r/g, `\r${padSpaceStr} `)
     text = `${padSpaceStr}${text}`
     text = text.slice(0, text.length - whitespace - 1)
-
+    text = text.replace(/\r$/, '')
     // 插入注释
     editBuilder.insert(insertPosition, text)
   })
